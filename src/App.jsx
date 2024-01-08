df9a31656a40777a9e86f8bdff655c129c3249ba
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-08 14:37:49 +0530

    Going through the code step by step and understanding the component tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
 
 function App() {
-  log('<App /> rendered');
-
+  log("<App /> rendered");
+  // step-1 : All the states are getting updated and the then the
+  //          functions are created on the first  render
   const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <>
+      {/* step-2: before excuting the whole code the Header componenet is executed first. */}
       <Header />
       <main>
         <section id="configure-counter">
@@ -28,6 +30,7 @@ function App() {
           <input type="number" onChange={handleChange} value={enteredNumber} />
           <button onClick={handleSetClick}>Set</button>
         </section>
+        {/* step-3: Counter componenet is then rendered. it passes the chosen count to the Counter component*/}
         <Counter initialCount={chosenCount} />
       </main>
     </>
