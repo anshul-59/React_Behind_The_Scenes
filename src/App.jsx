f8269825614b7aa47644732f4459ad3d6f73569a
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-08 16:04:11 +0530

    Using a clever components structure  practise so to avoid the use of memo function which actually  costs performance. A new ConfigureCounter component is created so that component will only render if ther prop that is use by that component is changed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,17 @@ import { useState } from "react";
 import Counter from "./components/Counter/Counter.jsx";
 import Header from "./components/Header.jsx";
 import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
   log("<App /> rendered");
   // step-1 : All the states are getting updated and the then the
   //          functions are created on the first  render
-  const [enteredNumber, setEnteredNumber] = useState(0);
+
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
   }
 
   return (
@@ -25,12 +21,7 @@ function App() {
       {/* step-2: before excuting the whole code the Header componenet is executed first. */}
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
-        {/* step-3: Counter componenet is then rendered. it passes the chosen count to the Counter component*/}
+        <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
